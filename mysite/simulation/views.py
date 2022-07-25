bb029a638f0b705e55a19092ca86c3309abedbb1
rohitrajuladevi <rohit.rajuladevi@gmail.com>
2022-07-25 16:51:09 -0400

    Added parameter selection and requirements

--- mysite/simulation/views.py
+++ mysite/simulation/views.py
@@ -7,20 +7,25 @@ import plotly.express as px
 
 
 def index(request):
-    cd = os.getcwd()
-    openfile = cd + '\\simulation\\tests\\IND_admin1_radiation_constant_0.05_normalized.patchsim'
-    popfile = cd + '\\simulation\\tests\\IND_admin1_population.patchsim'
-    cfgfile = cd + '\\simulation\\tests\\sample.cfg'
-    seedfile = cd + '\\simulation\\tests\\seed.txt'
+    
+    return render(request, 'simulation/index.html')
 
-    cfg = sim.read_config(cfgfile)
+def graph(request):
+    if request.method == 'POST':
+        exposure = request.POST["Exposure"]
+        cd = os.getcwd()
+        openfile = cd + '\\simulation\\tests\\IND_admin1_radiation_constant_0.05_normalized.patchsim'
+        popfile = cd + '\\simulation\\tests\\IND_admin1_population.patchsim'
+        cfgfile = cd + '\\simulation\\tests\\sample.cfg'
+        seedfile = cd + '\\simulation\\tests\\seed.txt'
 
-    cfg['PatchFile'] = popfile
-    cfg['NetworkFile'] = openfile
-    cfg['SeedFile'] = seedfile
-    dfh = sim.run_disease_simulation(cfg, return_epi=True,write_epi=True)
-    fig =px.line(dfh.sum())
-    fig.write_html(cd + "\\simulation\\templates\\simulation\\file.html")
-    context = {}
-    return render(request, 'simulation/file.html', context)
+        cfg = sim.read_config(cfgfile)
 
+        cfg['PatchFile'] = popfile
+        cfg['NetworkFile'] = openfile
+        cfg['SeedFile'] = seedfile
+        cfg['ExposureRate'] = exposure
+        dfh = sim.run_disease_simulation(cfg, return_epi=True,write_epi=True)
+        fig =px.line(dfh.sum())
+        fig.write_html(cd + "\\simulation\\templates\\simulation\\file.html")
+        return render(request, 'simulation/file.html')
